1dd73ed408dc73da6b72da8c9bcb5a0a101d30e5
justinknguyen <justinknguyen11@gmail.com>
2023-03-22 20:38:54 -0600

    moved functions in visualization to functions file

--- model/enel645_group5_functions.py
+++ model/enel645_group5_functions.py
@@ -1,7 +1,11 @@
 #Import required libraries
+import os
 import torch
 import torch.nn as nn
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision.models import vgg16_bn, VGG16_BN_Weights
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from torch_snippets import stems, read
@@ -51,8 +55,6 @@ class SegmentationDataset(Dataset):
 
 
         return images, ground_truth_masks
-    
-from torchvision.models import vgg16_bn, VGG16_BN_Weights
 
 # Define the convolutional block
 def conv(in_channels, out_channels):
@@ -81,7 +83,6 @@ class UNet(nn.Module):
         self.block4 = nn.Sequential(*self.encoder[20:27])
         self.block5 = nn.Sequential(*self.encoder[27:34])
 
-
         self.bottleneck = nn.Sequential(*self.encoder[34:])
         self.conv_bottleneck = conv(512, 1024)
 
@@ -100,15 +101,12 @@ class UNet(nn.Module):
     
     # Build up the forward pass from the components defined above
     def forward(self, x):
-        
-        
         # Left half of UNet - encoding
         block1 = self.block1(x)
         block2 = self.block2(block1)
         block3 = self.block3(block2)
         block4 = self.block4(block3)
         block5 = self.block5(block4)
-
 
         bottleneck = self.bottleneck(block5)
         x = self.conv_bottleneck(bottleneck)
@@ -118,28 +116,177 @@ class UNet(nn.Module):
         x = torch.cat([x, block5], dim=1)
         x = self.conv6(x)
 
-
         x = self.up_conv7(x)
         x = torch.cat([x, block4], dim=1)
         x = self.conv7(x)
-
 
         x = self.up_conv8(x)
         x = torch.cat([x, block3], dim=1)
         x = self.conv8(x)
 
-
         x = self.up_conv9(x)
         x = torch.cat([x, block2], dim=1)
         x = self.conv9(x)
-
 
         x = self.up_conv10(x)
         x = torch.cat([x, block1], dim=1)
         x = self.conv10(x)
 
-
         x = self.conv11(x)
 
-
         return x
+    
+def create_video(image_dir):
+    # Define the output video file name
+    output_file = "input_video.mp4"
+
+    # Get the list of image file names
+    image_files = sorted([file for file in os.listdir(image_dir) if file.endswith(".png")])
+    # Keep only the first half of the image files
+    image_files = image_files[:len(image_files) // 2]
+
+    # Read the first image to get its dimensions
+    first_image = cv2.imread(os.path.join(image_dir, image_files[0]))
+    height, width, _ = first_image.shape
+
+    # Set the video codec and create a VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_writer = cv2.VideoWriter(output_file, fourcc, 10, (width, height))
+
+    # Loop through the image files and add them as frames to the video
+    for file in image_files:
+        image_path = os.path.join(image_dir, file)
+        image = cv2.imread(image_path)
+        video_writer.write(image)
+
+    # Release the VideoWriter object
+    video_writer.release()
+
+def process_video(input_file, output_file, net, device):
+    cap = cv2.VideoCapture(input_file)
+
+    # Get video properties
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        # Resize the frame to match the required dimensions
+        resized_frame = cv2.resize(frame, (448, 448))
+
+        # Normalize the frame
+        image = get_transforms()(resized_frame.copy() / 255.)
+
+        # Convert the frame to a torch tensor
+        x_tensor = image[None].float().to(device)
+
+        # Predict the mask
+        with torch.no_grad():
+            _mask = net(x_tensor)
+            _, _mask = torch.max(_mask, dim=1)
+
+        # Apply the colormap to the predicted mask
+        num_classes = 30
+        _mask = _mask.squeeze().cpu().numpy()
+        mask_3d = np.repeat(_mask[:, :, np.newaxis], 3, axis=2)
+        color_mask = cv2.applyColorMap((mask_3d * 255 / (num_classes - 1)).astype(np.uint8), cv2.COLORMAP_JET)
+
+        # Resize the color mask back to the original frame size
+        color_mask = cv2.resize(color_mask, (width, height), interpolation=cv2.INTER_NEAREST)
+
+        # Combine the original frame and the color mask
+        output_frame = cv2.addWeighted(frame, 1, color_mask, 0.5, 0)
+
+        # Write the frame
+        out.write(output_frame)
+
+    # Release the resources
+    cap.release()
+    out.release()
+    
+def create_ground_truth_video(image_folder, mask_folder, output_file, fps=10):
+    image_files = sorted(os.listdir(image_folder))
+    image_files = image_files[:len(image_files) // 2]
+    mask_files = sorted(os.listdir(mask_folder))
+    mask_files = image_files[:len(mask_files) // 2]
+
+    # Get dimensions from the first image
+    first_image = cv2.imread(os.path.join(image_folder, image_files[0]))
+    height, width, _ = first_image.shape
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_writer = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
+
+    for image_file, mask_file in zip(image_files, mask_files):
+        # Read the ground truth mask
+        mask = cv2.imread(os.path.join(mask_folder, mask_file), cv2.IMREAD_GRAYSCALE)
+
+        # Combine the mask layers
+        num_classes = 30
+        combined_mask = np.zeros_like(mask)
+        for i in range(num_classes):
+            combined_mask += ((mask == i) * i * 255 / num_classes).astype(np.uint8)
+
+        # Apply a colormap to the combined mask
+        color_mask = cv2.applyColorMap(combined_mask.astype(np.uint8), cv2.COLORMAP_JET)
+
+        # Read the corresponding image and blend it with the color mask
+        image = cv2.imread(os.path.join(image_folder, image_file))
+        blended_frame = cv2.addWeighted(image, 1, color_mask, 0.5, 0)
+
+        # Write the blended frame to the video
+        video_writer.write(blended_frame)
+
+    # Release the resources
+    video_writer.release()
+    
+def display_frames_side_by_side(original_video, output_video, ground_truth_video, frame_indices):
+    original_cap = cv2.VideoCapture(original_video)
+    output_cap = cv2.VideoCapture(output_video)
+    gt_cap = cv2.VideoCapture(ground_truth_video)
+
+    total_frames = int(original_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for idx in frame_indices:
+        if idx < total_frames:
+            original_cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
+            output_cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
+            gt_cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
+
+            _, original_frame = original_cap.read()
+            _, output_frame = output_cap.read()
+            _, gt_frame = gt_cap.read()
+
+            # Convert frames to RGB format
+            original_frame = cv2.cvtColor(original_frame, cv2.COLOR_BGR2RGB)
+            output_frame = cv2.cvtColor(output_frame, cv2.COLOR_BGR2RGB)
+            gt_frame = cv2.cvtColor(gt_frame, cv2.COLOR_BGR2RGB)
+
+            # Plot the frames side-by-side
+            fig, axs = plt.subplots(1, 3, figsize=(30, 30))
+            axs[0].imshow(original_frame)
+            axs[0].set_title('Original Frame')
+            axs[1].imshow(gt_frame)
+            axs[1].set_title('Ground Truth Frame')
+            axs[2].imshow(output_frame)
+            axs[2].set_title('Predicted Frame')
+
+            # Remove axis ticks
+            for ax in axs:
+                ax.set_xticks([])
+                ax.set_yticks([])
+
+            plt.show()
+
+    original_cap.release()
+    output_cap.release()
+    gt_cap.release()
